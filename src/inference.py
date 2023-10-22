f52fbfaa31e6cfc724d4f723d670f139579b387c
Yvo Keller <keller.yvo@gmail.com>
2023-10-22 23:51:21 +0200

    train test model

--- src/inference.py
+++ src/inference.py
@@ -2,20 +2,23 @@ import torch
 from model import GPTLanguageModel
 import json
 
-config = json.load(open('../models/config.json'))
-hparams = config['hparams']
+CONFIG_PATH='../models/config.json'
+MODEL_PATH = '../models/gpt.pt'
 
-print(hparams['device'])
+config = json.load(open(CONFIG_PATH))
+hparams = config['hparams']
 
 # all the unique characters that occur in the text
 chars = config['chars']
 vocab_size = len(chars)
 
+# START --- taken from src/train_model.py ---
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
 itos = { i:ch for i,ch in enumerate(chars) }
 encode = lambda s: [stoi[c] for c in s] # encoder: take a string, output a list of integers
 decode = lambda l: ''.join([itos[i] for i in l]) # decoder: take a list of integers, output a string
+# END --- taken from src/train_model.py ---
 
 model = GPTLanguageModel(hparams['vocab_size'], hparams['n_emb_d'], hparams['n_heads'], hparams['n_layers'], hparams['dropout'], hparams['block_size'], hparams['device']).to(hparams['device'])
 model.load_state_dict(torch.load('../models/gpt.pt'))
--- src/train_model.py
+++ src/train_model.py
@@ -10,10 +10,9 @@ if mode == 'test':
     # TEST hparam config
     batch_size = 32 # how many independent sequences will we process in parallel?
     block_size = 8 # what is the maximum context length for predictions?
-    max_iters = 1
-    eval_interval = 50
+    max_iters = 5000
+    eval_interval = 500
     learning_rate = 1e-3
-    device = 'mps' if torch.backends.mps.is_available() else 'cpu'
     eval_iters = 200
     n_emb_d = 32 # number of embedding dimensions
     n_heads = 6
@@ -51,6 +50,8 @@ with open(dataset_path, 'r', encoding='utf-8') as f:
 # all the unique characters that occur in the text
 chars = sorted(list(set(text)))
 vocab_size = len(chars)
+
+print(chars)
 
 # create a mapping from characters to integers
 stoi = { ch:i for i,ch in enumerate(chars) }
@@ -91,6 +92,12 @@ def estimate_loss():
 model = GPTLanguageModel(vocab_size, n_emb_d, n_heads, n_layers, dropout, block_size, device)
 model = model.to(device)
 
+# Print total & trainable parameters
+pt_total_params = sum(p.numel() for p in model.parameters())
+pt_trainable_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f'Total parameters: {pt_total_params}')
+print(f'Trainable parameters: {pt_trainable_params}')
+
 # WandB – Initialize a new run
 wandb.login()
 run = wandb.init(
@@ -114,7 +121,7 @@ optimizer = torch.optim.AdamW(model.parameters(), lr=learning_rate)
 
 for iter in range(max_iters):
     # every once in a while evaluate the loss on train and val sets
-    if iter % eval_interval == 0:
+    if iter % eval_interval == 0 or iter == max_iters - 1:
         losses = estimate_loss()
         print(f"step {iter}: train loss {losses['train']:.4f}, val loss {losses['val']:.4f}")
         wandb.log({"train_loss": losses['train'], "val_loss": losses['val']})
@@ -131,7 +138,7 @@ for iter in range(max_iters):
 # save the model & config.json to disk with timestampf format YYYY-MM-DD_HH-MM-SS
 torch.save(model.state_dict(), f'../models/gpt.pt')
 
-with open("../models/config.json", "w") as f:
+with open("../models/config.json", "w", encoding='utf-8') as f:
     f.write(json.dumps({
             'hparams': model.config,
             'chars': chars,
